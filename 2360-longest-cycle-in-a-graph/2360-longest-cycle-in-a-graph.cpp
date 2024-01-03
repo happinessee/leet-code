798d9af36abba88f727eafd1662c79754f7a37c4
정효혁 <gygur1206@gmail.com>
2024-01-03 23:38:03 +0900

    Time: 140 ms (79.75%), Space: 94.1 MB (90.50%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -3,26 +3,30 @@ public:
     int longestCycle(vector<int>& edges) {
         int edgesLength = edges.size();
         vector<int> visited(edgesLength, -1);
+        vector<int> currentPath(edgesLength, -1);
         int longest = -1;
+        int index = 0, length = 0;
 
         for (int i = 0; i < edgesLength; i++) {
-            if (visited[i] != -1) continue;
+            if (visited[i] != -1 || edges[i] == -1) continue;
 
-            unordered_map<int, int> currentPath;
-            int index = i, length = 0;
+            index = i, length = 0;
 
-            while (index != -1 && visited[index] == -1) {
+            while (visited[index] == -1) {
                 visited[index] = i;
                 currentPath[index] = length;
                 index = edges[index];
                 length++;
 
-                if (index != -1 && visited[index] == i) {
+                if (index == -1) break;
+                else if (visited[index] == i) {
                     // 사이클 발견
                     longest = max(longest, length - currentPath[index]);
                     break;
                 }
+                
             }
+            //currentPath.clear();
         }
         return longest;
     }
