bd9c17ad864df5b0783ff76cce54544a54a1924e
정효혁 <gygur1206@gmail.com>
2024-03-17 18:45:21 +0900

    Time: 42 ms (58.95%), Space: 16.6 MB (47.35%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -2,15 +2,15 @@ class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
         n = len(nums)
         
-        # Compute the cumulative sums array
-        cumSum = [0] * n
+        # 누적합 배열을 생성합니다.
+        cumSum = [0 for _ in range(n)]
         for i in range(n):
             if i == 0:
                 cumSum[i] = nums[i]
             else:
                 cumSum[i] = cumSum[i - 1] + nums[i]
         
-        # Define 2D DP table
+        # 2차원 DP 배열을 생성합니다.
         dp = [[0 for _ in range(n)] for _ in range(n)]
         
         # Process for lengths of sub-array from 1 to n
@@ -19,13 +19,13 @@ class Solution:
                 start = j
                 end = i + j
                 
-                # Base Case 1
+                # 부분 배열의 원소가 1개일 때
                 if start == end:
                     dp[start][end] = nums[start]
-                # Base Case 2
+                # 부분 배열의 원소가 2개일 때
                 elif start + 1 == end:
                     dp[start][end] = max(nums[start], nums[end])
-                # Recursive Relationship
+                # 부분 배열의 원소가 3개 이상일 때
                 else:
                     choice_1 = nums[start] + (cumSum[end] - cumSum[start + 1] + nums[start + 1] - dp[start + 1][end])
                     choice_2 = nums[end] + (cumSum[end - 1] - cumSum[start] + nums[start] - dp[start][end - 1])
