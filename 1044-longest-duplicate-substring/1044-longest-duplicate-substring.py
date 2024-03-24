9397ab76d1c59587fb4f2b1a95aadec40aa0a2bc
정효혁 <gygur1206@gmail.com>
2024-03-24 19:06:45 +0900

    Time: 1975 ms (49.46%), Space: 239.4 MB (22.98%) - LeetHub

--- 1044-longest-duplicate-substring/1044-longest-duplicate-substring.py
+++ 1044-longest-duplicate-substring/1044-longest-duplicate-substring.py
@@ -1,56 +1,25 @@
 class Solution:
-    def build_partial_match_table(self, pattern: str):
-        pattern_length = len(pattern)
-        table = [0 for _ in range(pattern_length)]  # 부분 일치 테이블 초기화
-        j = 0  # 패턴의 접두사 끝 위치
+    def search_for_duplicate(self, s:str, length:int):
+        seen = set()
+        for i in range(len(s) - length + 1):
+            substring = s[i:i + length]
+            if substring in seen:
+                return i
+            seen.add(substring)
+        return -1
 
-        # i는 패턴의 접미사 끝 위치를 나타냄
-        for i in range(1, pattern_length):
-            # 현재 문자가 일치하지 않으면, j를 이전에 계산된 table[j-1] 위치로 이동
-            while j > 0 and pattern[i] != pattern[j]:
-                j = table[j-1]
-
-            # 현재 문자가 일치하면, 길이를 1 증가시키고 table에 기록
-            if pattern[i] == pattern[j]:
-                j += 1
-                table[i] = j
-
-        return table
-
-    def KMP_search(self, text:str, pattern:str):
-        """
-        KMP 알고리즘을 사용해 문자열에서 패턴을 검색하는 함수.
-
-        :param text: 검색 대상이 되는 전체 문자열
-        :param pattern: 찾고자 하는 패턴
-        :return: 
-        """
-        table = self.build_partial_match_table(pattern)
-        j = 0  # 패턴의 위치
-
-        for i in range(len(text)):
-            while j > 0 and text[i] != pattern[j]:
-                j = table[j-1]
-
-            if text[i] == pattern[j]:
-                if j == len(pattern)-1:  # 패턴의 끝에 도달
-                    return True
-                else:
-                    j += 1
-
-        return False
-    
-    def longestDupSubstring(self, s: str) -> str:
-        start = 0
-        end = 1
-        s_length = len(s)
-        substring = ""
+    def longestDupSubstring(self, s:str):
+        left, right = 1, len(s)
+        longest_sub = ""
         
-        while(end < s_length) :
-            if (s[start:end] in s[start+1:]):
-                substring = s[start:end]
-            else :
-                start += 1
-            end += 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            start = self.search_for_duplicate(s, mid)
             
-        return substring
+            if start != -1:  # 중복 부분 문자열 발견
+                longest_sub = s[start:start + mid]
+                left = mid + 1  # 더 긴 부분 문자열을 찾기 위해 범위를 늘림
+            else:
+                right = mid - 1  # 중복 부분 문자열이 없으므로 범위를 줄임
+        
+        return longest_sub
