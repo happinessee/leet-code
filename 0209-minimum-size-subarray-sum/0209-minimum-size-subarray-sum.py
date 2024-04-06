fe1f70e153af5b08333fe9feeb71ed96fe96662e
정효혁 <gygur1206@gmail.com>
2024-04-06 17:29:00 +0900

    Time: 210 ms (14.21%), Space: 30.4 MB (36.03%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -5,7 +5,6 @@ class Solution:
         prefix_sum_array[0] = nums[0]
         for i in range(1, nums_length):
             prefix_sum_array[i] = (prefix_sum_array[i-1] + nums[i])
-        print(prefix_sum_array)
         
         def subarray_sum(length: int):
             if (prefix_sum_array[length-1] >= target): return True
@@ -20,7 +19,6 @@ class Solution:
             
             while(st <= end):
                 mid = (st + end) // 2
-                print(st, end, mid)
                 if (subarray_sum(mid)):
                     end = mid - 1
                     result = mid
