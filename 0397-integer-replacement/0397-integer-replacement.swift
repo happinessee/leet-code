5624fd78d52f7f24742941324f7773dd486bddd7
정효혁 <gygur1206@gmail.com>
2024-05-20 20:40:21 +0900

    Time: 3 ms (90.91%), Space: 15.6 MB (9.09%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.swift
+++ 0397-integer-replacement/0397-integer-replacement.swift
@@ -1,36 +1,36 @@
 class Solution {
     func integerReplacement(_ n: Int) -> Int {
         var dp = Array(repeating: -1, count: 26)
-        dp[0] = 10000
-        dp[1] = 0
-        dp[2] = 1
-        dp[3] = 2
-        dp[4] = 2
-        dp[5] = 3
-        dp[6] = 3
-        dp[7] = 4
-        dp[8] = 3
-        dp[9] = 4
-        dp[10] = 4
-        dp[11] = 5
-        dp[12] = 4
-        dp[13] = 5
-        dp[14] = 5
-        dp[15] = 5
-        dp[16] = 4
-        dp[17] = 5
-        dp[18] = 5
-        dp[19] = 6
-        dp[20] = 5
-        dp[21] = 6
-        dp[22] = 6
-        dp[23] = 6
-        dp[24] = 5
-        dp[25] = 6
+        // dp[0] = 10000
+        // dp[1] = 0
+        // dp[2] = 1
+        // dp[3] = 2
+        // dp[4] = 2
+        // dp[5] = 3
+        // dp[6] = 3
+        // dp[7] = 4
+        // dp[8] = 3
+        // dp[9] = 4
+        // dp[10] = 4
+        // dp[11] = 5
+        // dp[12] = 4
+        // dp[13] = 5
+        // dp[14] = 5
+        // dp[15] = 5
+        // dp[16] = 4
+        // dp[17] = 5
+        // dp[18] = 5
+        // dp[19] = 6
+        // dp[20] = 5
+        // dp[21] = 6
+        // dp[22] = 6
+        // dp[23] = 6
+        // dp[24] = 5
+        // dp[25] = 6
         
         func replacement(_ n:Int) -> Int {
-            if (n <= 25) {
-                return dp[n]
+            if (n == 1) {
+                return 0
             }
             if n % 2 == 0 {
                 return replacement(n / 2) + 1
