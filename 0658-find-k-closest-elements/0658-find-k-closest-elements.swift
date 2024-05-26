e09650b9e0f90d0cff83d48dfb9ea5dd88ab5503
정효혁 <gygur1206@gmail.com>
2024-05-26 20:20:30 +0900

    Time: 127 ms (53.41%), Space: 16.3 MB (79.55%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.swift
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.swift
@@ -1,24 +1,31 @@
 class Solution {
-    func makeDistanceArray(_ arr: [Int], _ x: Int) -> [Int] {
-		var distanceArray = [Int]()
-		for i in 0..<arr.count {
-			distanceArray.append(abs(arr[i] - x))
-		}
-		return distanceArray
-    }
-    
-    func findClosestElements(_ arr: [Int], _ k: Int, _ x: Int) -> [Int] {
-		var distanceArray = makeDistanceArray(arr, x)
-		var result = [Int]()
-		var sortedArr = arr.enumerated().sorted { (a, b) in
-			if distanceArray[a.offset] == distanceArray[b.offset] {
-				return a.element < b.element
+	func findClosestElements(_ arr: [Int], _ k: Int, _ x: Int) -> [Int] {
+		var left = 0
+		var right = arr.count - 1
+		while left < right {
+			let mid = left + (right - left) / 2
+			if arr[mid] < x {
+				left = mid + 1
+			} else {
+				right = mid
 			}
-			return distanceArray[a.offset] < distanceArray[b.offset]
 		}
-		for i in 0..<k {
-			result.append(sortedArr[i].element)
+
+		// left와 right를 초기화하지만, 새로운 변수명 사용을 피하고 범위 검사 추가
+		right = left
+		left = left - 1
+
+		while right - left - 1 < k {
+			if left == -1 {  // left가 0 미만이 되는 것을 방지
+				right += 1
+			} else if right == arr.count {  // right가 배열 길이 이상이 되는 것을 방지
+				left -= 1
+			} else if left >= 0 && (x - arr[left]) <= (arr[right] - x) {  // left와 right 인덱스가 배열 범위 내에 있는지 확인
+				left -= 1
+			} else {
+				right += 1
+			}
 		}
-		return result.sorted()
-    }
+		return Array(arr[left+1...right-1])
+	}
 }
