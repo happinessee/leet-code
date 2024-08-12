48bbed0ca08a5796f180fa30835da6fd840f9c90
정효혁 <gygur1206@gmail.com>
2024-08-12 20:26:39 +0900

    Time: 814 ms (100%), Space: 30.9 MB (100%) - LeetHub

---
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.swift
@@ -0,0 +1,49 @@
+class UnionFind {
+    var parent: [Int]
+
+    public init(
+        size: Int
+    ) {
+        self.parent = Array(0..<size)
+    }
+    
+    public func union(_ x: Int, _ y: Int) {
+        let xi = find(x)
+        let yi = find(y)
+        parent[xi] = yi
+    }
+    
+    public func find(_ x: Int) -> Int {
+        if x == parent[x] {
+            return x
+        } else {
+            let xi = find(parent[x])
+            parent[x] = xi
+            return xi
+        }
+    }
+}
+
+class Solution {
+    func reachableNodes(_ n: Int, _ edges: [[Int]], _ restricted: [Int]) -> Int {
+        let unionFind = UnionFind(size: n)
+        let restrictedSet = Set(restricted)
+        for edge in edges {
+            let (u, v) = (edge[0], edge[1])
+
+            if !restrictedSet.contains(u) && !restrictedSet.contains(v) {
+                unionFind.union(v, u)
+            }
+        }
+        
+        // 0번 노드와 연결된 모든 노드를 센다
+        let root = unionFind.find(0)
+        for i in 0..<n {
+            if unionFind.parent[i] != root {
+                unionFind.find(i)
+            }
+        }
+        
+        return unionFind.parent.reduce(0) { $1 == root ? $0 + 1 : $0 }
+    }
+}
