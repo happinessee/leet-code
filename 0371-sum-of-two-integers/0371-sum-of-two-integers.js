ed68fd311bd41b6ea62113f4adced728b742ded1
정효혁 <gygur1206@gmail.com>
2024-10-19 20:00:56 +0900

    Time: 0 ms (100%), Space: 48.3 MB (92.33%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+    return Add(a, b);
+};
+
+var Add = function(a, b) {
+    while (b != 0) {
+        var sum = a ^ b;
+        var carry = (a & b) << 1;
+
+        a = sum;
+        b = carry;
+        // console.log("a: ", a);
+        // console.log("b: ", b);
+    }
+    return a;
+};
