ef98c5c189613a4dbd1651d61959531e268bd798
정효혁 <gygur1206@gmail.com>
2025-03-25 21:01:50 +0900

    Time: 15 ms (25.42%), Space: 58.2 MB (31.15%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.js
+++ 0994-rotting-oranges/0994-rotting-oranges.js
@@ -9,12 +9,15 @@ var orangesRotting = function(grid) {
     let elapsedMinutes = 0;
     let freshOrangesCount = checkFreshOrangesCount(grid, m, n);
 
+    if (freshOrangesCount === 0) return 0;
+
     const dirs = [[1,0], [-1,0], [0,1], [0,-1]];
     const visited = Array.from({ length: m }, () => Array(n).fill(0));
 
     while (true) {
         const rottenCells = [];
 
+        // 썩은 오렌지 칸 발견
         for (let i = 0; i < m; i++) {
             for (let j = 0; j < n; j++) {
                 if (grid[i][j] === 2 && visited[i][j] === 0) {
@@ -24,8 +27,10 @@ var orangesRotting = function(grid) {
             }
         }
 
+        // 썩은 오렌지가 없거나, 신선한 오렌지가 없다면 while문 탈출
         if (rottenCells.length === 0 || freshOrangesCount === 0) break;
 
+        // 썩은 오렌지 주변 칸 확산
         for (let i = 0; i < rottenCells.length; i++) {
             const x = rottenCells[i][0];
             const y = rottenCells[i][1];
