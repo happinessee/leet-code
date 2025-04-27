2bce44e57153792f2db2236d184f812207e350a9
정효혁 <gygur1206@gmail.com>
2025-04-27 16:50:35 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.ts
@@ -0,0 +1,42 @@
+function increasingTriplet(nums: number[]): boolean {
+    if (nums.length < 3) return false;
+    if ((new Set(nums)).length < 3) return false;
+
+    let i = 0;
+    let j = 1;
+    let k = 2;
+
+    const length = nums.length;
+
+    while(i < length - 2) {
+        console.log(i, j, k)
+        if (nums[i] < nums[j] && nums[j] < nums[k]) return true;
+
+        if (k === length - 1 && j === length - 2) {
+            i++;
+            j = i + 1;
+            k = i + 2;
+            continue;
+        }
+
+        if (k === length - 1 && j !== length - 2) {
+            j++;
+            k = j + 1;
+            continue;
+        } 
+
+        if (nums[i] >= nums[j]) {
+            i++;
+            j = i + 1;
+            k = i + 2;
+            continue;
+        }
+
+        if (nums[j] >= nums[k]) {
+            k++;
+            continue;
+        }
+    }
+
+    return false;
+};
