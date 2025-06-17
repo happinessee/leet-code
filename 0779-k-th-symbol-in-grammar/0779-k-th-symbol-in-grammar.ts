acab0f397715931d1c7c1c9946f6d024286ec14d
정효혁 <gygur1206@gmail.com>
2025-06-17 20:13:39 +0900

    Time: 1 ms (0%), Space: 54.7 MB (64.71%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.ts
@@ -0,0 +1,32 @@
+
+/// 0으로 시작해서, 모든 0은 01로, 모든 1은 10으로 변경
+/// 계속 후속이 따라오도록 함
+
+// 만약 n이 3이고 k가 5 (1,2,3,4,5,6,7,8)
+// 그러면 n은 2이고 k가 4의 반대를 적으면 됨.
+// 0 - 1
+// 01 - 2
+// 0110 - 3
+// 01101001 - 4
+// 0110100110010110 - 5
+// 01101001100101101001011001101001 - 6
+
+function kthGrammar(n: number, k: number): number {
+    const table = ['0', '01', '0110', '01101001', '0110100110010110', '01101001100101101001011001101001']
+    
+    if (n < 7 || k < 33) return parseInt(table[5][k-1]);
+
+    let positive = true;
+    while(n >= 7 && k >= 33) {
+        n--;
+        if (k > (2 ** (n-1))) {
+            k = k - (2 ** (n-1));
+            positive = !positive;
+        }
+    }
+    console.log(n, k, positive)
+    const result1 = parseInt(table[5][k-1]) === 1; // 1이면 true 0이면 false
+    const result2 = positive ? result1 : !result1
+
+    return result2 ? 1 : 0
+};
